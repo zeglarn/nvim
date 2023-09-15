e21ff04d62d36ff5682a0a9e107a4f1e5bdd9c84
Alexander Zeijlon <alexander.zeijlon@cendio.se>
2023-09-15 15:57:09 +0200

    Add lsp plugins config
    This one is really not finished yet.

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,4 +1,13 @@
 return {
+  {
+    "neovim/nvim-lspconfig",
+  },
+  {
     "williamboman/mason.nvim",
     config = true,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = true,
+  },
 }
